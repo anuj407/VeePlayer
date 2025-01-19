ef0d29f0fada239651beb3f57cd676a533accbf0
anuj407 <anuj97953a@gmail.com>
2025-01-19 14:35:49 +0530

    Mongoose Connection

---
+++ Server/src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try{
+        const connectionInstace= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connected DB !! HOST : ${connectionInstace.connection.host}`);
+    }
+    catch(err){
+        console.error(`Error connecting to MongoDB: ${err.message} `);
+        process.exit(1);
+    }
+} 
+
+export default connectDB;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,1 +1,11 @@
-console.log("It's done")
+import express from 'express';
+import dotenv from 'dotenv';
+import db from './db/db.js';
+const app = express();
+dotenv.config()
+db()
+app.get('/', function(req,res){
+    res.send("Home")
+})
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
