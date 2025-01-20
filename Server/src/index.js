ff1d51b0798b04982a3f18cda883f8ad50f2aa07
anuj407 <anuj97953a@gmail.com>
2025-01-20 23:21:27 +0530

    setup cors origin

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import db from './db/db.js';
 const app = express();
 dotenv.config()
 db()
+
+app.use(cors({
+   origin: process.env.CORS_ORIGIN,
+   credentials: true
+}))
+app.use(express.json({limit: "20kb"}))
+app.use(express.urlencoded({ extended: true , limit: "20kb"  }))
+app.use(express.static('public'))
+app.use(cookieParser())
 app.get('/', function(req,res){
     res.send("Home")
 })
