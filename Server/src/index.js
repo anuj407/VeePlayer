24aa0e20643206de8cc7634cfcca79937f2b0933
anuj407 <anuj97953a@gmail.com>
2025-01-22 21:43:35 +0530

    Routes setups

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,22 +1,10 @@
-import express from 'express';
+
 import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import db from './db/db.js';
-const app = express();
+import { app } from './app.js';
 dotenv.config()
 db()
-
-app.use(cors({
-   origin: process.env.CORS_ORIGIN,
-   credentials: true
-}))
-app.use(express.json({limit: "20kb"}))
-app.use(express.urlencoded({ extended: true , limit: "20kb"  }))
-app.use(express.static('public'))
-app.use(cookieParser())
-app.get('/', function(req,res){
-    res.send("Home")
+.then(()=>{
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 })
-
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+ .catch((err) => console.error(err));
--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 
 const asyncHandler = (requestHandler)=>{
-    (req, res, next) =>{
+  return  (req, res, next) =>{
         Promise.resolve(requestHandler(req, res, next)).catch((err)=>{
             console.error(err)
             res.status(500).json({message: 'Server Error'})
@@ -8,6 +8,7 @@ const asyncHandler = (requestHandler)=>{
     }
 }
 
+export {asyncHandler}
 // Another method
 // const asyncHandler = (requestHandler) =>()=>{
 //     return async (req, res) => {
