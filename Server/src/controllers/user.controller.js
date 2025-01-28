c22ad2fa411cc7925901eca8613caf9cdc3df073
anuj407 <anuj97953a@gmail.com>
2025-01-28 10:14:39 +0530

    add getWatchHistory controller

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+import { pipeline } from "stream";
 
 // Generate Refresh Token and Access Token
 const generateAccessAndRefreshToken = async (userid) =>{
@@ -171,6 +173,11 @@ const changePassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "Password changed successfully"))
 })
 
+// Get Current User Profile
+ const getCurrentUserProfile = asyncHandler(async (req, res) => {
+    return res.status(200).json(new ApiResponse(200,"User profile retrieved successfully",req.user))
+})
+
 // Update User Account
 const updateUserAccount = asyncHandler(async (req,res)=>{
      const {fullName,email}= req.body
@@ -193,7 +200,7 @@ const updateUserAccount = asyncHandler(async (req,res)=>{
 
 // Update User Avatar
  const updateUserAvatar = asyncHandler(async(req,res)=>{
-    const avatarLocalPath = req.files['avatar'][0].path
+    const avatarLocalPath = req.file?.path
     if(!avatarLocalPath){
         throw new apiError("Avatar is required", 400)
     }
@@ -212,7 +219,7 @@ const updateUserAccount = asyncHandler(async (req,res)=>{
  })
 // Update User CoverImage
  const updateUserCoverImage = asyncHandler(async(req,res)=>{
-    const coverImageLocalPath = req.files['coverImage'][0]?.path
+    const coverImageLocalPath = req.file?.path
     if(!coverImageLocalPath){
         throw new apiError("CoverImage is required", 400)
     }
@@ -230,7 +237,7 @@ const updateUserAccount = asyncHandler(async (req,res)=>{
     .json(new ApiResponse(200,"User CoverImage updated successfully",updatedUser))
  })
 // Get User Profile
-const getUserProfile = asyncHandler(async (req,res)=>{
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
     const username= req.params;
     if(!username?.trim()){
         throw new apiError('Username is required', 400)
@@ -288,15 +295,53 @@ const getUserProfile = asyncHandler(async (req,res)=>{
     .status(200)
     .json(new ApiResponse(200,"User Profile retrieved successfully",channel[0]))
 })
+
+// GetWatchHistory
+ const getWatchHistory = asyncHandler(async (req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{_id: new mongoose.Types.ObjectId(req.user._id)}
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory"
+            },
+            pipeline:[{
+                $lookup:{
+                    from:"user",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"owner"
+                },
+                pipeline:[{
+                    $project:{
+                        fullName:1,
+                        username:1,
+                        avatar:1
+                       
+                    }
+                }]
+            }]
+        }
+    ])
+    return res
+    .status(200)
+    .json(new ApiResponse(200,"Watch History retrieved successfully",user[0].watchHistory))
+})
 export {
     registerUser, 
     loginUser, 
     logoutUser , 
     refreshAccessToken,
     changePassword,
-    getUserProfile,
+    getCurrentUserProfile,
     updateUserAccount,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
 
 }
