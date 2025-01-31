9f44fe3b3bd75fc4dbbe120c11e9c6113fb4ecae
anuj407 <anuj97953a@gmail.com>
2025-01-31 18:45:15 +0530

    Playlist controllers , routes Testing and Debuging

--- Server/src/routes/playlist.routes.js
+++ Server/src/routes/playlist.routes.js
@@ -3,12 +3,12 @@ import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, re
 import { verifyJwt } from "../middlewares/auth.middleware.js"
 
 const router = Router()
-router.use(verifyJwt)
-router.route("/create-playlist/:videoId").post(createPlaylist)
+
+router.route("/create-playlist/:videoId").post(verifyJwt,createPlaylist)
 router.route("/update-playlist/:playlistId").patch(updatePlaylist)
 router.route("/delete-playlist/:playlistId").delete(deletePlaylist)
 router.route("/add-video-to-playlist/:playlistId/:videoId").post(addVideoToPlaylist)
 router.route("/remove-video-from-playlist/:playlistId/:videoId").patch(removeVideoFromPlaylist)
-router.route("get-playlist/:playlistId").get(getPlaylistById)
+router.route("/get-playlist/:playlistId").get(getPlaylistById)
 
 export default router;
