60c64499391d853086313483c52afd67c4273ac5
anuj407 <anuj97953a@gmail.com>
2025-02-03 18:53:04 +0530

    Update Cors Origin

--- Server/src/app.js
+++ Server/src/app.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT","PATCH", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"]
  }))
  app.use(express.json({limit: "20kb"}))
  app.use(express.urlencoded({ extended: true , limit: "20kb"  }))
