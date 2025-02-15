b487f841d57c3a9c12e0fc7769ee7436de6498a9
anuj407 <anuj97953a@gmail.com>
2025-02-15 10:12:46 +0530

    modify category and sidebar

--- Client/src/components/Category.jsx
+++ Client/src/components/Category.jsx
@@ -11,23 +11,17 @@ function Category() {
         setTotalScroll(containerRef.current.scrollWidth)
     };
   return (
-    <div className="h-16 bg-black flex items-center pl-10 gap-x-8 fixed top-18 z-20">
-        <div className="text-4xl bg-zinc-900">
-           <assets.IoMenuOutline />
-        </div>
-        <div className="w-[92vw] pr-4 flex items-center">
-           <button className={`text-4xl w-8 h-8 rounded-full cursor-pointer flex items-center justify-center hover:bg-[#222222]`}> <assets.MdChevronLeft className={`${scroll<1 && `hidden`}`}/></button>
-            <div id="category" ref={containerRef} onScroll={()=>handleScroll()} className="flex gap-x-3 px-2 rounded-full overflow-x-scroll ">
+   <>
+        <button className={`text-4xl w-8 h-8 rounded-full cursor-pointer flex items-center justify-center hover:bg-[#222222]`}> <assets.MdChevronLeft className={`${scroll<1 && `hidden`}`}/></button>
+        <div id="category" ref={containerRef} onScroll={()=>handleScroll()} className="flex gap-x-3 px-2 rounded-full overflow-x-scroll ">
             {
                 category.map((item,index)=>
                     <button key={index} onClick={()=>setSearchCategory(index)} className={`${searchCategory == index ? `bg-white text-black`:`bg-[#222222] hover:bg-[#3f3f3f]`} px-3 h-8 cursor-pointer rounded-md min-w-fit`}>{item}</button>
                 )
             }
-            </div>
-            <button className="text-4xl w-8 h-8 rounded-full cursor-pointer flex items-center justify-center hover:bg-[#222222]"><assets.MdChevronRight className={`${scroll<totalScroll-window.innerWidth*0.88 ? `block`:"hidden"}`}/></button>
         </div>
-       
-    </div>
+        <button className="text-4xl w-8 h-8 rounded-full cursor-pointer flex items-center justify-center hover:bg-[#222222]"><assets.MdChevronRight className={`${scroll<totalScroll-window.innerWidth*0.88 ? `block`:"hidden"}`}/></button>
+   </>
   )
 }
 
--- Client/src/components/SideBar.jsx
+++ Client/src/components/SideBar.jsx
@@ -2,16 +2,19 @@ import { assets } from "../assets/assets"
 function SideBar() {
   return (
     <>
-        <div className="w-28 h-[85.5vh] bg-black flex flex-col gap-10 text-sm fixed top-28">
-            <div className="mt-10 w-full flex flex-col items-center cursor-pointer">
+        <div className="w-28 h-[85.5vh] bg-black flex flex-col gap-8 text-sm fixed top-20 ">
+        <div className="w-2/3 mx-auto h-12 flex flex-col items-center justify-center cursor-pointer hover:bg-[#3f3f3f] rounded-xl">
+           <assets.IoMenuOutline className="text-3xl" />
+        </div>
+            <div className="w-2/3 mx-auto h-16 flex flex-col items-center justify-center cursor-pointer hover:bg-[#3f3f3f] rounded-xl">
                 <assets.LiaHomeSolid className="text-3xl"/>
                 <h4 className="text-center">Home</h4>
             </div>
-            <div className="w-full flex flex-col items-center cursor-pointer">
+            <div className="w-8/9 mx-auto h-16 flex flex-col items-center justify-center cursor-pointer hover:bg-[#3f3f3f] rounded-xl">
                 <assets.PiVideo className="text-3xl"/>
                 <h4 className="text-center">Subscriptions</h4>
             </div>
-            <div className=" w-full flex flex-col items-center cursor-pointer">
+            <div className="w-2/3 mx-auto h-16 flex flex-col items-center justify-center cursor-pointer hover:bg-[#3f3f3f] rounded-xl">
                 <assets.IoPersonCircleOutline className="text-3xl"/>
                 <h4 className="text-center">You</h4>
             </div>
