684ed6a1175beefa9c1b686801c606b84ff9ee91
anuj407 <anuj97953a@gmail.com>
2025-02-24 16:11:35 +0530

    fetch Channel Data and get

--- Client/src/pages/Channel.jsx
+++ Client/src/pages/Channel.jsx
@@ -1,7 +1,20 @@
+import { useDispatch, useSelector } from "react-redux"
 import Navbar from "../components/Navbar"
 import SideBar from "../components/SideBar"
+import { getChannel } from "../store/Reducers/ChannelSlice"
+import { useEffect } from "react"
+import VideoCard from "../components/VideoCard"
+import { fetchVideos } from "../store/Reducers/VideoSlice"
 
 function Channel() {
+
+  const {fullName,email,username,avatar,coverImage,subscribersCount,} = useSelector(getChannel)
+  console.log(fullName,email,username,avatar,coverImage);
+  const { videos } = useSelector((state) => state.videos);
+  const dispatch = useDispatch()
+  useEffect(()=>{
+      dispatch(fetchVideos())
+  },[dispatch])
   return (
     <>
         <div className="w-full min-h-screen">
@@ -15,24 +28,29 @@ function Channel() {
                     <div className="w-[98%] mx-auto h-[10rem] bg-red-500 rounded-2xl"></div>
                     <div className="w-full h-[11rem] border flex">
                         <div className="w-[12%]">
-                           <div className="w-[10rem] h-[10rem] bg-green-500 rounded-full">
-                               <h4>Full Name</h4>
+                           <div className="w-[10rem] h-[10rem] rounded-full overflow-hidden ">
+                              <img className="w-full h-full object-cover" src={avatar} alt="" />
+                           </div>
+                        </div>
+                        <div className="w-fit h-full ">
+                        <h4>{fullName}</h4>
                                <div className="">
-                                  <span>Username</span>
-                                  <span>Subscribers</span>
-                                  <span>Total Videos</span>
+                                <span>{username} </span>
+                                  <span>. Subscribers {subscribersCount}</span>
+                                  <span>. Total Videos</span>
                                </div>
                                <div className="">Description</div>
                                <div className="">
                                 <button>Button 1</button>
                                 <button>Button 2</button>
                                </div>
-                           </div>
                         </div>
-                        <div className="w-fit h-full "></div>
                     </div>
                     <div className=""></div>
                 </div>
+            </div>
+            <div className="hidden">
+               {videos.map((video,index)=><VideoCard key={index} video={video} />)}
             </div>
         </div>
     </>
