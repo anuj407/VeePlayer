c25ed6bdd87b386ae9e556f17696aba4e02eff67
anuj407 <anuj97953a@gmail.com>
2025-02-26 01:25:25 +0530

    add one more userVideo reducer

--- Client/src/store/Store.jsx
+++ Client/src/store/Store.jsx
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit"
 import VideoReducer from "./Reducers/VideoSlice"
 import UserReducer from "./Reducers/UserSlice"
 import ChannelReducer from "./Reducers/ChannelSlice"
+import UserVideosReducer from "./Reducers/UserVideoSlice"
 export const store = configureStore({
    
     reducer: {
         videos: VideoReducer,
         user: UserReducer,
-        channel: ChannelReducer
+        channel: ChannelReducer,
+        userVideos:UserVideosReducer
     },
 })
