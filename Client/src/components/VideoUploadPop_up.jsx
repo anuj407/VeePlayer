163d7eb2d27dbb9ca4aba3b4c1c1085c3b15124a
anuj407 <anuj97953a@gmail.com>
2025-03-05 00:39:24 +0530

    create buttons

--- Client/src/components/VideoUploadPop_up.jsx
+++ Client/src/components/VideoUploadPop_up.jsx
@@ -83,46 +83,48 @@ function VideoUploadPop_up({updateParent}) {
   const {getRootProps, getInputProps} = useDropzone({onDrop})
   return (
     <>
-      <div className={`w-[65vw] h-[90vh] bg-[#282828] rounded-3xl flex flex-col ${isUploading ? `gap-7` : `gap-12`}`}>
-          <div className="h-[8%] border-b flex justify-between items-center p-2 px-6 font-medium text-[1.1rem]">
+      <div className={`w-[65vw] h-[90vh] bg-[#282828] rounded-3xl flex flex-col ${isUploading ? `gap-7` : `gap-12`} `}>
+          <div className="h-[8%] border-b border-[#8f8f8f] flex justify-between items-center p-2 px-6 font-medium text-[1.1rem]">
             <h1 className="">{fileName == "Select Files" ? `Upload Video` : `${fileName}`}</h1>
             <div onClick={updateParent} className="text-2xl cursor-pointer">
                 <assets.RxCross2 />
             </div>
           </div>
           {isUploading ?
-              <div className="flex h-[83%] flex-col items-center gap-y-6 overflow-y-scroll scrollbar-hide">
-                <div className="w-[80%] h-[10%] flex justify-between relative">
-                  <div onClick={()=>HandleSteps("Details")} className={`${titleValid ? `text-white` : `text-red-400`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
-                    <h1 >Details</h1>
-                    {step == "Details" ? <assets.FaCircleDot className="text-2xl"/> :
-                    <assets.IoIosCheckmarkCircle className="text-xl"/>
-                    }
-                  </div>
-                  <div onClick={()=>HandleSteps("Video")} className={`${Progress >= 1  ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
-                    <h1>Video Element</h1>
-                    {step == "Video" ? <assets.FaCircleDot className="text-2xl"/> :
-                    <assets.FaCircleDot className="text-xl"/>
-                    }
-                  </div>
-                  <div onClick={()=>handleChecks()} className={`${checks ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
-                    <h1>Checks</h1>
-                    {checks ? <assets.IoIosCheckmarkCircle className="text-2xl"/> :
-                    <assets.FaCircleDot className={`${step == "Checks" ?`text-2xl` : `text-xl`}`}/>
-                    }
-                  </div>
-                  <div onClick={()=>HandleSteps("Visibility")} className={`${Progress >= 3  ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
-                    <h1>Visibility</h1>
-                    {step == "Visibility" ? <assets.FaCircleDot className="text-2xl"/> :
-                    <assets.FaCircleDot className={"text-xl"}/>
-                    }
-                  </div>
-                  <div className={`w-[27%] h-[2px] ${Progress >= 1  ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[8%] top-[70%] z-0`}></div>
-                  <div className={`w-[27%] h-[2px] ${Progress >= 2 ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[36.5%] top-[70%] z-0`}></div>
-                  <div className={`w-[27%] h-[2px] ${Progress >= 3 ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[65%] top-[70%] z-0`}></div>
+              <div className="flex h-[83%] flex-col items-center gap-y-6 overflow-y-scroll scrollbar-hide relative overflow-x-hidden">
+                <div className="w-[60%] h-[8%] fixed bg-[#282828] z-20">
+                  <div className="w-full  h-full flex justify-between relative">
+                    <div onClick={()=>HandleSteps("Details")} className={`${titleValid ? `text-white` : `text-red-400`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
+                      <h1 >Details</h1>
+                      {step == "Details" ? <assets.FaCircleDot className="text-2xl"/> :
+                      <assets.IoIosCheckmarkCircle className="text-xl"/>
+                      }
+                    </div>
+                    <div onClick={()=>HandleSteps("Video")} className={`${Progress >= 1  ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
+                      <h1>Video Element</h1>
+                      {step == "Video" ? <assets.FaCircleDot className="text-2xl"/> :
+                      <assets.FaCircleDot className="text-xl"/>
+                      }
+                    </div>
+                    <div onClick={()=>handleChecks()} className={`${checks ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
+                      <h1>Checks</h1>
+                      {checks ? <assets.IoIosCheckmarkCircle className="text-2xl"/> :
+                      <assets.FaCircleDot className={`${step == "Checks" ?`text-2xl` : `text-xl`}`}/>
+                      }
+                    </div>
+                    <div onClick={()=>HandleSteps("Visibility")} className={`${Progress >= 3  ? `text-[#b0adad]` :`text-[#575656]`} font-medium w-[15%] hover:bg-[#3f3f3fab] cursor-pointer flex flex-col items-center justify-center gap-y-1 z-10`}>
+                      <h1>Visibility</h1>
+                      {step == "Visibility" ? <assets.FaCircleDot className="text-2xl"/> :
+                      <assets.FaCircleDot className={"text-xl"}/>
+                      }
+                    </div>
+                    <div className={`w-[27%] h-[2px] ${Progress >= 1  ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[8%] top-[70%] z-0`}></div>
+                    <div className={`w-[27%] h-[2px] ${Progress >= 2 ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[36.5%] top-[70%] z-0`}></div>
+                    <div className={`w-[27%] h-[2px] ${Progress >= 3 ? `bg-[#b0adad]` :`bg-[#8c8b8b80]`} absolute left-[65%] top-[70%] z-0`}></div>
 
+                  </div>
                 </div>
-                <div className="w-[90%] min-h-[90%]">
+                <div className="w-[90%] min-h-[90%] mt-[8%]">
                   <div className="w-[65%] h-full flex flex-col gap-6">
                     <div className="flex justify-between items-center">
                       <h1 className={` text-2xl font-semibold`}>Details</h1>
@@ -144,6 +146,15 @@ function VideoUploadPop_up({updateParent}) {
                     </div>
                   </div>
                   <div className=""></div>
+                </div>
+                <div className="w-[65.5%] h-[7%] bottom-[5%] fixed border-t-[1px] border-[#8f8f8f] flex justify-between items-center">
+                  <div className="ml-4">
+                  icons
+                  </div>
+                  <div className="w-[15%] flex justify-evenly items-center">
+                    <button className={`${step == "Details" ? `hidden` : `block`} px-3 py-1.5 bg-[#3f3f3f] text-white font-medium text-sm rounded-2xl cursor-pointer`}>Back</button>
+                    <button className="px-3 py-1.5 bg-white text-black font-medium text-sm rounded-2xl cursor-pointer">Next</button>
+                  </div>
                 </div>
               </div>
            :
