7f5e7e7488e11b4a9d88b54b7da190185de0d6a7
anuj407 <anuj97953a@gmail.com>
2025-03-08 08:25:06 +0530

    fixed issue

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -25,18 +25,18 @@ function Home() {
   return (
     <div className="min-h-screen">
         <Navbar/>
-        <div onClick={handleHomeButton} className="w-[7%] fixed left-0 top-[5rem] h-12 flex items-center justify-center ">
+        <div onClick={handleHomeButton} className="w-[5.5rem] fixed left-0 top-[5rem] h-12 flex items-center justify-center ">
            <assets.IoMenuOutline className="w-[80%] h-[80%] py-1 hover:bg-[#3f3f3f] cursor-pointer  rounded-xl" />
         </div>
-        <div className={`${fullSideBar ? `w-[16%] px-4`:`w-[6%]`} bg-black flex flex-col gap-8 text-[0.8rem] fixed top-32 py-3`}>
+        <div className={`${fullSideBar ? `w-[16%] px-4`:`w[5rem]`} bg-black flex flex-col gap-8 text-[0.8rem] fixed top-32 py-3 `}>
            <SideBar fullSideBar={fullSideBar} />
         </div>
-        <div className="left-[7vw] h-16 bg-black flex items-center gap-x-8 fixed top-18 z-20">     
+        <div className="left-[5.5rem]  h-16 bg-black flex items-center gap-x-8 fixed top-18 z-20">     
             <div className="w-[92vw] pr-4 flex items-center">                  
                 <Category/>
             </div>      
         </div>
-        <div className={`py-3 pl-1 mt-32 ${fullSideBar ? `ml-[16%]` :`ml-[6%]`} flex flex-wrap gap-4`}>
+        <div className={`py-3 pl-1 mt-32 ${fullSideBar ? `ml-[16%]` :`ml-[5.8rem]`} flex flex-wrap gap-4`}>
         {
           videos?.map((video,index)=><VideoCard key={index} index={index} video={video}/>)
         }
