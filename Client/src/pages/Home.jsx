06ed605d8e03c4036ea7835f372461da970247ae
anuj407 <anuj97953a@gmail.com>
2025-03-29 00:58:15 +0530

    modify home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -24,27 +24,38 @@ function Home() {
   if (status === 'failed') return <p>Error: {error}</p>;
   return (
     <div className="min-h-screen">
+      <div className="w-full h-[5rem]">
         <Navbar/>
-        <div onClick={handleHomeButton} className="w-[5.5rem] fixed left-0 top-[5rem] h-12 flex items-center justify-center ">
-           <assets.IoMenuOutline className="w-[80%] h-[80%] py-1 hover:bg-[#3f3f3f] cursor-pointer  rounded-xl" />
-        </div>
-        <div className={`${fullSideBar ? `w-[16%] px-4`:`w[5rem]`} bg-black flex flex-col gap-8 text-[0.8rem] fixed top-32 py-3 `}>
-           <SideBar fullSideBar={fullSideBar} />
-        </div>
-        <div className="left-[5.5rem]  h-16 bg-black flex items-center gap-x-8 fixed top-18 z-20">     
+      </div>
+        <div onClick={handleHomeButton} className="w-[100%] h-12 flex items-center  ">
+          <div className="w-[5rem] h-full">
+            <div className="fixed left-[2%]">
+              <assets.IoMenuOutline className="w-[2.5rem] h-full text-3xl py-1 hover:bg-[#3f3f3f] cursor-pointer  rounded-xl" />
+            </div>
+          </div>
+           <div className="left-[5.5rem] h-16 bg-black flex items-center gap-x-8 fixed top-18 z-20">     
             <div className="w-[92vw] pr-4 flex items-center">                  
                 <Category/>
             </div>      
         </div>
-        <div className={`py-3 pl-1 mt-32 ${fullSideBar ? `ml-[16%]` :`ml-[5.8rem]`} flex flex-wrap gap-4`}>
-        {
-          videos?.map((video,index)=>
-          <div key={index} className="w-[24%] h-[18rem]">
-            <VideoCard index={index} video={video}/>
-          </div>
-        )
-        }
         </div>
+        <div className="w-full  flex">
+            <div className={`${fullSideBar ? `w-[12rem]`:`w-[5.5rem]`} h-[44rem] bg-black flex flex-col gap-8 text-[0.8rem] top-32 py-3 `}>
+              <div className="fixed">
+              <SideBar fullSideBar={fullSideBar} />
+              </div>
+            </div>
+            <div className={`py-3 w-[95%] pl-2 flex flex-wrap gap-4`}>
+            {
+              videos?.map((video,index)=>
+              <div key={index} className="w-[24%] h-[18rem]">
+                <VideoCard index={index} video={video}/>
+              </div>
+            )
+            }
+            </div>
+        </div>
+        
     </div>
   )
 }
