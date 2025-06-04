bf178918355e6a0b9bf6e80b95d40f3dff23a6af
anuj407 <anuj97953a@gmail.com>
2025-06-04 15:09:18 +0530

    refactor LikedVideos reducer to implement async thunk for fetching liked videos and streamline state management

--- Client/src/store/Reducers/LikedVideos.jsx
+++ Client/src/store/Reducers/LikedVideos.jsx
@@ -1,21 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { apiUrl } from "../../utils/constants";
+
+export const getLikedVideos = createAsyncThunk(
+    'likedVideos/getLikedVideos',
+     async()=>{
+        try{
+            const response = await fetch(`${apiUrl}/likes/liked-videos`, {
+                method:"GET",
+                credentials:"include",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+            const result = await response.json()
+            return result.data 
+        }
+        catch(error){
+            console.log(error)
+        }
+     }
+
+)
 
 const LikedVideos = createSlice({
     name: "likedVideos",
     initialState: {
         likedVideos: [],
     },
-    reducers: {
-        addLikedVideo: (state, action) => {
-            state.likedVideos.push(action.payload);
-        },
-        removeLikedVideo: (state, action) => {
-            state.likedVideos = state.likedVideos.filter(
-                (video) => video.id !== action.payload.id
-            );
-        },
-    },
+    reducers: {},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(getLikedVideos.pending,(state)=>{
+            state.status = "loading"
+        })
+        .addCase(getLikedVideos.fulfilled,(state,action)=>{
+            state.status = "succeeded",
+            state.likedVideos = action.payload
+        })
+        .addCase(getLikedVideos.rejected,(state,action)=>{
+            state.status = "failed",
+            state.error = action.error.message
+        })
+
+    }
 })
-export const { addLikedVideo, removeLikedVideo } = LikedVideos.actions;
 export const selectLikedVideos = (state) => state.likedVideos.likedVideos;
 export default LikedVideos.reducer;
