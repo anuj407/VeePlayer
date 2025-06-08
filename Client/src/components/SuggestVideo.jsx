437404bfecf093dfab50e31b961c55f023e4f9f7
anuj407 <anuj97953a@gmail.com>
2025-06-08 10:14:32 +0530

    remove commented-out code from SuggestVideo component for cleaner codebase

--- Client/src/components/SuggestVideo.jsx
+++ Client/src/components/SuggestVideo.jsx
@@ -1,29 +1,16 @@
-import { useDispatch, useSelector } from "react-redux"
-import Category from "./category"
-import { fetchVideos, selectVideos } from "../store/Reducers/VideoSlice"
-import { useEffect } from "react"
-import HVideoCard from "./HVideoCard"
+// import { useDispatch, useSelector } from "react-redux"
+// import Category from "./category"
+// import { fetchVideos, selectVideos } from "../store/Reducers/VideoSlice"
+// import { useEffect } from "react"
+// import HVideoCard from "./HVideoCard"
 
-function SuggestVideo() {
-  const dispatch = useDispatch()
-  const {videos,status} = useSelector(selectVideos)
-  useEffect(()=>{
-       if(status === 'idle'){
-          dispatch(fetchVideos())
-       }
-  },[dispatch, status])
-  return (
-    <>
-      <div className="w-[25%] h-full flex flex-col gap-6">
-        <div className="flex ">
-           <Category/>
-        </div>
-        <div className="flex flex-col gap-3 h-[8rem]">
-          {videos.map((video,index)=><HVideoCard key={index} video={video} />)}
-        </div>
-      </div>
-    </>
-  )
-}
+// function SuggestVideo() {
+ 
+//   return (
+//     <>
+      
+//     </>
+//   )
+// }
 
-export default SuggestVideo
+// export default SuggestVideo
