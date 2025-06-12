ed9a40d12be7d0b35cbbb95255708cf8aab33225
anuj407 <anuj97953a@gmail.com>
2025-06-12 15:28:39 +0530

    refactor Channel component to enhance video fetching logic, improve layout, and streamline user video presentation

--- Client/src/pages/Channel.jsx
+++ Client/src/pages/Channel.jsx
@@ -1,34 +1,46 @@
 import { useDispatch, useSelector } from "react-redux"
 import Navbar from "../components/Navbar"
 import SideBar from "../components/SideBar"
-import { getChannel } from "../store/Reducers/ChannelSlice"
+import { fetchProfile, getChannel } from "../store/Reducers/ChannelSlice"
 import { useEffect, useState } from "react"
 import HVideoCard from "../components/HVideoCard"
 import VideoCard from "../components/VideoCard"
 import Channel_HomeVideo from "../components/Channel_HomeVideo"
-import { fetchUserVideos, selectUserVideos } from "../store/Reducers/UserVideoSlice"
+import { fetchUserVideos } from "../store/Reducers/UserVideoSlice"
 import { fetchVideos } from "../store/Reducers/VideoSlice"
+import { useParams } from "react-router-dom"
 
 function Channel() {
 
-  const {_id,fullName,username,avatar,coverImage,subscribersCount,} = useSelector(getChannel)
-  const {videos} = useSelector(selectUserVideos)
+  const {_id,fullName,avatar,coverImage,subscribersCount,} = useSelector(getChannel)
+  const userVideos = useSelector((state) => state.userVideos.videos);
   const dispatch = useDispatch()
+  const {username}=useParams()
+
   const [select,setSelect]= useState(0)
-  useEffect(()=>{
-      dispatch(fetchVideos())
-      if(_id){
-        dispatch(fetchUserVideos(_id))
-      }
-  },[dispatch,_id])
+  // Fetch Videos
+  useEffect(() => {
+    dispatch(fetchVideos());
+    if (_id) {
+      dispatch(fetchUserVideos(_id));
+    }
+  }, [dispatch, _id]);
+
+  // Effect for profile only when userVideos has data
+  useEffect(() => {
+      const params = { username: username, userId: userVideos[0]?.owner?._id };
+      dispatch(fetchProfile(params));
+  }, [dispatch, userVideos, username]);
   return (
     <>
         <div className="w-full min-h-screen overflow-hidden">
             <div className="w-screen h-[5rem] "></div>
             <Navbar/>
             <div className="flex">
-                <div className="w-[7.5rem] min-h-[46rem] relative">
-                  <SideBar/>
+                <div className="w-[5.5rem] min-h-[46rem]">
+                  <div className="fixed">
+                    <SideBar/>
+                  </div>
                 </div>
                 <div className="w-full flex flex-col gap-2 items-center ">
                     <div className="w-[90%] border mx-auto h-[10rem] rounded-2xl overflow-hidden">
@@ -60,7 +72,7 @@ function Channel() {
                     {select == 0 &&               
                       <div className="w-[90%] py-6 flex flex-col items-center gap-6 ">  
                           <div className="w-full mx-auto ">            
-                            {videos.map((video,index)=>
+                            {userVideos?.map((video,index)=>
                               <div key={index} className={`w-4/6 h-[16rem] ${index == 1 ? `block`:`hidden`}`}>
                                 <HVideoCard  video={video} />
                               </div>
@@ -69,8 +81,8 @@ function Channel() {
                           <div className="w-full h-[0.8px] bg-[#616161]"></div>
                           <div className="w-full">
                               <h1 className="text-xl font-semibold" >For You</h1>
-                              <div className="flex mt-2">
-                                {videos.map((video,index)=><div key={index} className={`${index < 3 ? `block`:`hidden`}`}><VideoCard video={video}/></div>
+                              <div className="flex gap-3 mt-2 h-[16rem]">
+                                {userVideos?.map((video,index)=><div key={index} className={`${index < 3 ? `block`:`hidden`} w-[23%]`}><VideoCard video={video}/></div>
                                 )}
                               </div>
                           </div>
@@ -83,7 +95,7 @@ function Channel() {
                     {/* Videos              */}
                     {select == 1 &&
                     <div className="w-[90%] flex mt-2 gap-2">
-                     { videos.map((video, index)=><VideoCard key={index} video={video}/>)}
+                     { userVideos?.map((video, index)=><VideoCard key={index} video={video}/>)}
                     </div>
                     }
                 </div>
